dcb1372aece464b265b88dd863130f8a3a1e2aea
gamze <gamzekandara@gmail.com>
2025-01-23 20:01:16 -0500

    Stack methods implemented

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,46 @@
+# these are the operations that can be performedon Stack.?
+
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=[], limit=100):
+        self.items = items if items else []
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        # returns true if the Stack is empty; false otherwise
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        # before you appending check if stack is full
+        if self.full():  # true/false
+            return None
+        #overflowError
+        self.items.append(item)
 
     def pop(self):
-        pass
+        # check if empty. if empty , you can not pop item.
+        if self.isEmpty():
+            return None
+        return self.items.pop()
 
     def peek(self):
-        pass
-    
+        # top element
+        if self.isEmpty():
+            return -1
+        else:
+            return self.items[-1]
+
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        # limit is 100
+        return len(self.items) >= self.limit
+
 
     def search(self, target):
-        pass
+        # Check if the target is in the stack
+        if target in self.items:
+            # Distance from the top (0-based index)
+            return len(self.items) - 1 - self.items.index(target)
+        return -1  # Element not found
